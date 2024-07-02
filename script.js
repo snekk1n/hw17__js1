d2c98bc00c45bcebd6c7bf738c66123c49e1fee1
snekk1n <n.ashymbekov2005@gmail.com>
2024-07-02 12:46:13 +0300

    hello

---
+++ script.js
@@ -0,0 +1,27 @@
+// let a=4
+// let b=55
+// plus = a+ b
+// minus = b - a
+// delenie = b / a
+// ostatok = b % a
+// console.log(plus, minus, delenie, ostatok)
+
+// let hey = 'Hello!'
+// let name = 'Nurdan'
+// console.log(hey , name)
+
+// let number = 5
+// let string = 'asd'
+// console.log(number/string)
+
+// let i = 5
+// let j = +'5'
+// console.log(i + j)
+
+// let c = 2
+// let u //false
+// console.log(!c == !!u) //true потому что, false == false
+// console.log(!c == !u) //false потому что, false == true
+// console.log(c == !u) //false потому что, number == true
+// console.log(1 == !u) //true потому что, true == true
+// console.log(!!c == !u) //true потому что, true == true
